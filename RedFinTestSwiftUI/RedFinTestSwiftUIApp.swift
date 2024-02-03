71f5bebeaf8164988907d12b4186ba6543bb8fd7
Jael <jael.ruvalcaba@uabc.edu.mx>
2024-02-02 22:43:00 -0800

    Initial Commit

---
+++ RedFinTestSwiftUI/RedFinTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedFinTestSwiftUIApp.swift
+//  RedFinTestSwiftUI
+//
+//  Created by Jael on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RedFinTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
